188771b43a13df4fb16d2df3abd5efb684fcf66b
Serge Yudin <enfros2000@gmail.com>
2019-03-15 16:23:35 +0300

    login page

--- skin/frontend/tendergreens/default/js/scripts.js
+++ skin/frontend/tendergreens/default/js/scripts.js
@@ -1,77 +1,15 @@
-$(document).ready(function () {
-    $('.header_user').on('click', function () {
-        $('.header_user-menu').slideToggle()
+/* eslint prefer-arrow-callback: 0 */
+
+$(document).ready(function() {
+    $(".service_orders__tr").on("click", function() {
+        $(this).find(".service_orders_openclose").toggleClass("opened");
+        $(this).closest(".service_orders__tr").find(".collapse").collapse('toggle');
     });
 
-    $('.header_burger').on('click', function () {
-        $('body').toggleClass('leftpanel-opened')
+    $(".asidelinks").on("click", function() {
+        $(this).toggleClass("opened");
     });
 
-    $('.aside_mobile_bg').on('click', function () {
-        $('body').removeClass('leftpanel-opened')
-    });
 
-    $('.rightpanel-opener').on('click', function () {
-        $('.main_section').toggleClass('rightpanel-opened')
-    });
 
-    $('.rightpanel-mobilebg').on('click', function () {
-        $('.main_section').removeClass('rightpanel-opened')
-    });
-
-    $('.aside_list-list .checkbox').on('change', function () {
-        $(this).parent().toggleClass('checked');
-    });
-
-    $('.aside_list-title').on('click', function () {
-        $(this).parent().children('.aside_list-list').slideToggle();
-        $(this).parent().toggleClass('active');
-    });
-
-    $('.category_page-images').masonry({
-        itemSelector: '.category_page-img',
-        columnWidth: '.category_page-img-grid',
-        horizontalOrder: true,
-    })
-
-    $('.catalog2-star').on('click', function () {
-        $(this).toggleClass('selected');
-    });
-
-    $('.catalog2-star').on({
-        mouseenter: function () {
-            $('a.catalog2_item_icon').on('click', function () {
-                return false;
-            });
-        },
-        mouseleave: function () {
-            $('a.catalog2_item_icon').off('click');
-        }
-    });
-
-    $('.catalog2_item_menu label.checkbox').on('click', function () {
-        if ($(this).children('input').prop('checked')) {
-            $(this).children('input').prop('checked', false);
-            $(this).closest('.catalog2_item').removeClass('selected');
-        } else {
-            $(this).children('input').prop('checked', true);
-            $(this).closest('.catalog2_item').addClass('selected')
-            $('.catalog2_item').addClass('menuvisible')
-        }
-        if ($('.selected').length < 1) {
-            $('.catalog2_item').removeClass('menuvisible')
-        };
-    });
-    
-    $('.catalog2_item_menu-opener').on('click', function () {
-        $(this).toggleClass('opened');
-        $(this).parent().toggleClass('menuopened');
-        $(this).parent().children('.catalog2_menu').slideToggle();
-    });
 });
-
-$(window).on('load resize', function () {
-    $('.main-content').css('min-height', $(window).height() - $('header').height() + 'px');
-    
-});
-
